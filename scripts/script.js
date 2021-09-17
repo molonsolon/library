bd068bfe2ffce97f2f7c53f061ce7a864410c64e
Joseph Coulter <joeycoulter@gmail.com>
2021-09-17 16:10:32 -0700

    Starting CSS over, replacing with Bootstrap

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,15 @@
 let myLibrary = [];
 const body = document.querySelector(`body`);
-const bookContainer = document.querySelector(`#book-container`)
+const bookContainer = document.querySelector(`#book-container`);
+const addBookButton = document.querySelector(`#new-book-btn`);
+const addBookForm = document.querySelector(`#new-book-form`)
+const eastOfEden = addBookToLibrary(`East of Eden`, `John Steinbeck`, `612`, `read `);
+const theSoundAndTheFury = addBookToLibrary(`The Sound and the Fury`, `William Faulkner`, `217`, `read `);
+const bannerOfHeaven = addBookToLibrary(`Under The Banner of Heaven`, `Jon Krakauer`, `434`, `not read yet `);
+const theHobbit = addBookToLibrary(`The Hobbit`, `J.R.R. Tolkien`, `295`, `read `);
+const formElements = addBookForm.elements;
+const formClose = document.querySelector(`#form-close`); 
+const formSubmit = document.querySelector(`#new-book-submit`);
 
 
 function Book(title, author, pages, read) {
@@ -9,15 +18,105 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`
-    }
+        bookArray = [title, `by`, `${author},`, pages, `pages,`, read];
+        return bookArray.join(` `);
+
+    };
+    this.toggleRead = function() {
+        console.log(this.read);
+        if (this.read === `not read yet `) {
+            this.read = `read `;
+            bookArray = [title, `by`, `${author},`, pages, `pages,`, `read`];
+            return bookArray.join(` `);
+            
+        } else if (this.read === `read `) {
+            this.read = `not read yet `;
+            bookArray = [title, `by`, `${author},`, pages, `pages,`, `not read yet`];
+            return bookArray.join(` `);
+        } else {
+            this.read === `ERROR`;
+            return alert(`ERROR`);
+        }
+    };
+    
 };
 
+
+
+function addBookToLibrary(title, author, pages, read) {
+    newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
 // Book.prototype.info = function() {
 //     return `${title} by ${author}, ${pages} pages, ${read}`;
 // };
 
-const theHobbit = addBookToLibrary(`The Hobbit`, `J.R.R. Tolkien`, `295`, `read`);
+function addNewBook (title, author, pages, read) {
+    newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    const book = document.createElement(`div`);
+    book.classList.toggle('book');
+    book.textContent += newBook.info();
+    book.setAttribute(`data-book-number`, `${myLibrary.length}`)
+    bookContainer.appendChild(book);
+}
+
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const book = document.createElement(`div`);
+        book.classList.toggle('book');
+        book.setAttribute(`data-book-id`, `${i}`);
+        
+        const bookTitle = document.createElement(`p`);
+        bookTitle.textContent = myLibrary[i].info();
+        bookTitle.setAttribute(`data-book-title`, `${i}`);
+        bookTitle.classList.toggle(`book-title`);
+
+
+        const removeBookBtn = document.createElement(`button`);
+        removeBookBtn.classList.toggle(`remove-book-btn`);
+        removeBookBtn.textContent = `x`;
+        
+        const readButton =  document.createElement(`button`);
+        readButton.setAttribute(`id`,`read-btn`);
+        readButton.textContent = `read status`;
+        
+        
+        bookContainer.appendChild(book);
+        book.appendChild(bookTitle)
+        book.appendChild(removeBookBtn);
+        book.appendChild(readButton);
+
+        removeBookBtn.addEventListener(`click`, () => {
+            bookContainer.removeChild(book);
+        });
+
+        readButton.addEventListener(`click`, () => bookTitle.textContent = myLibrary[i].toggleRead());
+    };
+};
+
+
+
+
+addBookButton.addEventListener(`click`, () => {
+    addBookForm.classList.toggle(`form-off`);
+
+});
+
+formClose.addEventListener(`click`, () => {
+    addBookForm.classList.toggle(`form-off`);
+
+})
+
+
+
+formSubmit.addEventListener(`click`, () => {
+    const formTitle = addBookForm.elements[1].value;
+    const formAuthor = addBookForm.elements[2].value;
+    const formPages = addBookForm.elements[3].value;
+    const formReadRadio = addBookForm.elements.read;
+    addNewBook(formTitle, formAuthor, formPages, formReadRadio.value);
+})
 
 
 // function addBookToLibrary(title, author, pages, read)) {
@@ -25,23 +124,7 @@ const theHobbit = addBookToLibrary(`The Hobbit`, `J.R.R. Tolkien`, `295`, `read`
 //     myLibrary.push(newBook);
 // }
 
-function addBookToLibrary(title, author, pages, read) {
-    newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-}
 
-const eastOfEden = addBookToLibrary(`East of Eden`, `John Steinbeck`, `612`, `read`);
-const theSoundAndTheFury = addBookToLibrary(`The Sound and the Fury`, `William Faulkner`, `217`, `read`);
-const bannerOfHeaven = addBookToLibrary(`Under The Banner of Heaven`, `Jon Krakauer`, `434`, `not read yet`);
 
-function displayBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        console.log(myLibrary[i]);
-        const book = document.createElement(`div`);
-        book.classList.toggle('book');
-        book.textContent += myLibrary[i].info();
-        bookContainer.appendChild(book);
-        
-    };
-}
+
 displayBooks();
