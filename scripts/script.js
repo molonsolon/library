ae555714850d8dd64ef3afe104e1a374b4892bee
Joseph Coulter <joeycoulter@gmail.com>
2021-09-18 15:45:42 -0700

    Improved book card creation script

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,9 @@ const eastOfEden = addBookToLibrary(`East of Eden`, `John Steinbeck`, `612`, `re
 const theSoundAndTheFury = addBookToLibrary(`The Sound and the Fury`, `William Faulkner`, `217`, `read `);
 const bannerOfHeaven = addBookToLibrary(`Under The Banner of Heaven`, `Jon Krakauer`, `434`, `not read yet `);
 const theHobbit = addBookToLibrary(`The Hobbit`, `J.R.R. Tolkien`, `295`, `read `);
+const chaosBook = addBookToLibrary(`Chaos`, `Tom O'Neill`, `528`, `not read yet `)
 const formElements = addBookForm.elements;
-const formClose = document.querySelector(`#form-close`); 
+const formClose = document.querySelector(`#form-close`);
 const formSubmit = document.querySelector(`#new-book-submit`);
 let rowNumber = 0;
 
@@ -17,18 +18,18 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
+    this.info = function () {
         bookArray = [title, `by`, `${author},`, pages, `pages,`, read];
         return bookArray.join(` `);
 
     };
-    this.toggleRead = function() {
+    this.toggleRead = function () {
         console.log(this.read);
         if (this.read === `not read yet `) {
             this.read = `read `;
             bookArray = [title, `by`, `${author},`, pages, `pages,`, `read`];
             return bookArray.join(` `);
-            
+
         } else if (this.read === `read `) {
             this.read = `not read yet `;
             bookArray = [title, `by`, `${author},`, pages, `pages,`, `not read yet`];
@@ -38,7 +39,7 @@ function Book(title, author, pages, read) {
             return alert(`ERROR`);
         }
     };
-    
+
 };
 
 
@@ -51,56 +52,51 @@ function addBookToLibrary(title, author, pages, read) {
 //     return `${title} by ${author}, ${pages} pages, ${read}`;
 // };
 
-function addNewBook (title, author, pages, read) {
+function addNewBook(title, author, pages, read) {
     newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
-    const book = document.createElement(`div`);
-    book.classList.toggle('book');
-    book.textContent += newBook.info();
-    book.setAttribute(`data-book-number`, `${myLibrary.length}`)
-    
-    
-    bookContainer.appendChild(book);
+    while (bookContainer.hasChildNodes()) {
+        bookContainer.removeChild(bookContainer.lastChild);
+    }
+    displayBooks();
 }
 
 function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
-        
-        
+
         if (i % 4 === 0) {
-        const bookRow = document.createElement(`div`);
-        bookRow.classList.toggle(`row`);
-        rowNumber += 1;
-        bookRow.setAttribute(`book-row-${rowNumber}`)
+            const addBookRow = document.createElement(`div`);
+            addBookRow.classList.toggle(`row`);
+            rowNumber += 1;
+            addBookRow.setAttribute(`id`, `book-row-${rowNumber}`);
+            bookContainer.appendChild(addBookRow);
         }
-        
-        
-        
+
+        const bookRow = document.getElementById(`book-row-${rowNumber}`);
 
         const book = document.createElement(`div`);
         book.classList.toggle('book');
         book.classList.toggle(`card`);
         book.classList.toggle(`col-lg`);
         book.setAttribute(`data-book-id`, `${i}`);
-        
+
         const bookTitle = document.createElement(`p`);
         bookTitle.textContent = myLibrary[i].info();
         bookTitle.setAttribute(`data-book-title`, `${i}`);
         bookTitle.classList.toggle(`book-title`);
         bookTitle.classList.toggle(`card-header`)
 
-
         const removeBookBtn = document.createElement(`button`);
         removeBookBtn.classList.toggle(`remove-book-btn`);
         removeBookBtn.textContent = `x`;
         removeBookBtn.classList.toggle(`card-footer`)
-        
-        const readButton =  document.createElement(`button`);
-        readButton.setAttribute(`id`,`read-btn`);
+
+        const readButton = document.createElement(`button`);
+        readButton.setAttribute(`id`, `read-btn`);
         readButton.textContent = `read status`;
         readButton.classList.toggle(`card-footer`);
-       
 
+        bookRow.appendChild(book);
         book.appendChild(bookTitle)
         book.appendChild(removeBookBtn);
         book.appendChild(readButton);
