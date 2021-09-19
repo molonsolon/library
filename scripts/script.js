1531afc05eddcb2cbd8a94e1da2b4af1a86ec6d0
Joseph Coulter <joeycoulter@gmail.com>
2021-09-19 13:50:08 -0700

    Add DOMPurify, general styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,21 @@
+
+
+
 let myLibrary = [];
 const body = document.querySelector(`body`);
 const bookContainer = document.querySelector(`#book-container`);
 const addBookButton = document.querySelector(`#new-book-btn`);
 const addBookForm = document.querySelector(`#new-book-form`)
-const eastOfEden = addBookToLibrary(`East of Eden`, `John Steinbeck`, `612`, `read `);
-const theSoundAndTheFury = addBookToLibrary(`The Sound and the Fury`, `William Faulkner`, `217`, `read `);
-const bannerOfHeaven = addBookToLibrary(`Under The Banner of Heaven`, `Jon Krakauer`, `434`, `not read yet `);
-const theHobbit = addBookToLibrary(`The Hobbit`, `J.R.R. Tolkien`, `295`, `read `);
-const chaosBook = addBookToLibrary(`Chaos`, `Tom O'Neill`, `528`, `not read yet `)
+const eastOfEden = addBookToLibrary(`East of Eden`, `John Steinbeck`, `612`, `Read `);
+const theSoundAndTheFury = addBookToLibrary(`The Sound and the Fury`, `William Faulkner`, `217`, `Read `);
+const bannerOfHeaven = addBookToLibrary(`Under The Banner of Heaven`, `Jon Krakauer`, `434`, `Not read yet `);
+const theHobbit = addBookToLibrary(`The Hobbit`, `J.R.R. Tolkien`, `295`, `Read `);
+const chaosBook = addBookToLibrary(`Chaos`, `Tom O'Neill`, `528`, `Not read yet `)
 const formElements = addBookForm.elements;
 const formClose = document.querySelector(`#form-close`);
 const formSubmit = document.querySelector(`#new-book-submit`);
 let rowNumber = 0;
+
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -19,21 +23,21 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function () {
-        bookArray = [title, `<br>`, `by`, author, `<br>`, pages, `pages,`, `<br>`, read];
-        return bookArray.join(` `);
+        bookArray = [title, `<br>`, `by`, author, `<br>`, pages, `pages`, `<br>`, read];
+        return DOMPurify.sanitize(bookArray.join(` `));
 
     };
     this.toggleRead = function () {
         console.log(this.read);
-        if (this.read === `not read yet `) {
-            this.read = `read `;
-            bookArray = [title, `by`, `${author},`, pages, `pages,`, `read`];
-            return bookArray.join(` `);
+        if (this.read === `Not read yet `) {
+            this.read = `Read `;
+            bookArray = [title, `<br>`, `by`, author, `<br>`, pages, `pages`, `<br>`, `Read`];
+            return DOMPurify.sanitize(bookArray.join(` `));
 
-        } else if (this.read === `read `) {
-            this.read = `not read yet `;
-            bookArray = [title, `by`, `${author},`, pages, `pages,`, `not read yet`];
-            return bookArray.join(` `);
+        } else if (this.read === `Read `) {
+            this.read = `Not read yet `;
+            bookArray = [title, `<br>`, `by`, author, `<br>`, pages, `pages`, `<br>`, `Not read yet`];
+            return DOMPurify.sanitize(bookArray.join(` `));
         } else {
             this.read === `ERROR`;
             return alert(`ERROR`);
@@ -84,10 +88,11 @@ function displayBooks() {
         book.setAttribute(`data-book-id`, `${i}`);
 
         const bookTitle = document.createElement(`p`);
-        bookTitle.textContent = myLibrary[i].info();
+        bookTitle.innerHTML = myLibrary[i].info();
         bookTitle.setAttribute(`data-book-title`, `${i}`);
         bookTitle.classList.toggle(`book-title`);
-        bookTitle.classList.toggle(`card-header`)
+        bookTitle.classList.toggle(`text-center`);
+        
 
         const removeBookBtn = document.createElement(`button`);
         removeBookBtn.classList.toggle(`remove-book-btn`);
@@ -104,10 +109,10 @@ function displayBooks() {
         book.appendChild(readButton);
 
         removeBookBtn.addEventListener(`click`, () => {
-            bookContainer.removeChild(book);
+            bookRow.removeChild(book);
         });
 
-        readButton.addEventListener(`click`, () => bookTitle.textContent = myLibrary[i].toggleRead());
+        readButton.addEventListener(`click`, () => bookTitle.innerHTML = myLibrary[i].toggleRead());
     };
 };
 
